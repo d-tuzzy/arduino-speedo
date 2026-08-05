cc6c77507793a1ceaa33783a01f57ae18e614793
Daniel Turczyniak <danturczyniak@gmail.com>
2026-08-05 19:52:04 +0100

    Throttle Input Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
 #include <Arduino.h>
 
+const int throttlePin = A0;
 const int ledPin = 3;
 
 void setup() {
+  Serial.begin(115200); // Allows communication between computer at 115200 bits/second
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  for (int i = 0; i <= 255; i++)
-  {
-    analogWrite(ledPin, i);
-    delay(5);
-  }
+  int input = analogRead(throttlePin);
 
-  for (int i = 255; i >= 0; i--)
-  {
-    analogWrite(ledPin, i);
-    delay(5);
-  }
+  Serial.print(input);
+
+  int brightness = map(input, 168, 726, 0, 255); // Scale input to 0-255 range (proportionally)
+  brightness = constrain(brightness, 0, 255); // Prevent values escaping the range
+
+  analogWrite(ledPin, brightness);
+  delay(50);
 }
