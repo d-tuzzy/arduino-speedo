e4e9d3e826d3465303fa1c31855d6bb5fccea32e
d-tuzzy <danturczyniak@gmail.com>
2026-09-03 21:22:47 +0100

    Speed in mph test

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,54 @@
 #include <Arduino.h>
 
-const int throttlePin = A0;
-const int ledPin = 3;
+const int hallPin = 2;
+
+// The wheel's circumference is 0.78m and there are 15 pulses per revolution
+const float distancePerPulse = 0.78 / 15.0;
+
+volatile unsigned long lastPulse = 0; // Time of the previous sensor pulse
+volatile unsigned long pulseTime = 0; // Time between the last two pulses
+
+void pulse() {
+  unsigned long now = micros(); // Get current time in microseconds
+
+  if (now - lastPulse < 2000) {
+    return; // Ignore pulses that are too close together
+  }
+
+  pulseTime = now - lastPulse; // Calculate time since previous pulse
+  lastPulse = now; // Save time of this pulse
+}
 
 void setup() {
-  Serial.begin(115200); // Allows communication between computer at 115200 bits/second
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+
+  pinMode(hallPin, INPUT_PULLUP); // Set Hall sensor pin as an input
+
+  // Run pulse() whenever the Hall sensor changes from HIGH to LOW
+  attachInterrupt(digitalPinToInterrupt(hallPin), pulse, FALLING);
 }
 
 void loop() {
-  int input = analogRead(throttlePin);
+  unsigned long timeSincePulse;
+  unsigned long interval;
 
-  Serial.print(input);
+  // Temporarily stop interrupts while copying these values
+  noInterrupts();
 
-  int brightness = map(input, 168, 726, 0, 255); // Scale input to 0-255 range (proportionally)
-  brightness = constrain(brightness, 0, 255); // Prevent values escaping the range
+  timeSincePulse = micros() - lastPulse;
+  interval = pulseTime;
 
-  analogWrite(ledPin, brightness);
-  delay(50);
+  interrupts();
+
+  float speed = 0;
+
+  // If a pulse has been received recently, calculate speed in mph
+  if (interval > 0 && timeSincePulse < 1000000) {
+    // Calculates m/s and then converts to mph
+    speed = (distancePerPulse / (interval / 1000000.0)) * 2.23694;
+  }
+
+  Serial.println(speed, 1); // Print speed to 1 d.p.
+
+  delay(100); // Update speed every 100 ms
 }
